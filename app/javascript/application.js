78c3a0cd66f2381003e7a6dc27a5879f4ff2ce19
Russell Hallowes-Smith <russellsmith6@gmail.com>
2023-07-18 18:46:55 +0100

    modal data needs work

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,4 +2,4 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import "bootstrap"
-import "./controllers/browse"
+import "./controllers/browse.js"
--- app/javascript/controllers/browse.js
+++ app/javascript/controllers/browse.js
@@ -1,18 +1,51 @@
+console.log("connected")
 const list = document.querySelector("#results")
 
 const insertMovies = (data) => {
   data.Search.forEach((result) => {
-    const movieTag = `<li class="list-group-item border-0">
-      <img src="${result.Poster}" alt="" width="100">
-    </li>`
-    list.insertAdjacentHTML("beforeend", movieTag)
-  })
-}
+    const movieTag = `<div class="card">
+      <img src="${result.Poster}" alt="">
+      <div class="content">
+        <h6> ${result.Title} </h6>
+        <p> Type: ${result.Type} </p>
+        <p> Release Year: ${result.Year} </p>
+        <button type="button" class="btn btn-primary modal-btn" data-bs-toggle="modal" data-bs-target="#exampleModal" data-imdbid="${result.imdbID}">
+          More Info
+        </button>
+      </div>
+    </div>`;
+    list.insertAdjacentHTML("beforeend", movieTag);
+  });
+
+  list.addEventListener("click", (event) => {
+    const target = event.target;
+    // Check if the clicked element is the "More Info" button
+    if (target.classList.contains("modal-btn")) {
+      const imdbID = target.getAttribute("data-imdbid");
+      const modalContent = document.querySelector(".modal-body");
+      modalContent.innerText = imdbID;
+    }
+  });
+};
+
+
+// const setupModalButtonClickListeners = () => {
+//   const modalContent = document.querySelector(".modal-body");
+//   const movieData = document.querySelectorAll(".modal-btn");
+
+//   movieData.forEach((element) => {
+//     element.addEventListener("click", (event) => {
+//       console.log("hello");
+//       modalContent.innerText = "hello";
+//     });
+//   });
+// };
 
 const fetchMovies = (query) => {
   fetch(`http://www.omdbapi.com/?s=${query}&apikey=adf1f2d7`)
     .then(response => response.json())
     .then(data => insertMovies(data))
+    // .then(() => setupModalButtonClickListeners());
 }
 
 fetchMovies("harry potter") // on 1st page load
@@ -23,4 +56,4 @@ form.addEventListener("submit", (event) => {
   list.innerHTML = ""
   const input = document.querySelector("#search-input")
   fetchMovies(input.value)
-})
+});
